b452048c735133f05f8fe3d7d9e374fa0c0473bc
Jacek Jakubik <36626288+jjdv@users.noreply.github.com>
2019-01-02 15:49:51 +0100

    preloading of the rest of the planets data

--- src/app/components/planet-list/planet-list.component.ts
+++ src/app/components/planet-list/planet-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { Store, select } from '@ngrx/store';
 
 import { Initialize, GetAll } from '../../ngrx/planets/planets.actions'
@@ -12,17 +12,26 @@ import { PlanetsData, planetsT } from 'src/app/services/planets/planets.config';
   styleUrls: ['./planet-list.component.scss']
 })
 export class PlanetListComponent implements OnInit {
+  private storePlanetsData$: Observable<PlanetsData>;
   planetsList$: Observable<planetsT>;
 
-  constructor(private store: Store<{ planetData: PlanetsData }>) {}
+  constructor(private store$: Store<{planetsData: PlanetsData}>) {
+    this.storePlanetsData$ = store$.pipe( select('planetsData') );
+  }
 
   ngOnInit() {
-    this.planetsList$ = this.store.pipe( select('planetsData'), map(planetsData => planetsData.planets) );
-    this.store.dispatch(new Initialize())
+    this.planetsList$ = this.storePlanetsData$.pipe( map(planetsData => planetsData.planets) );
+    this.store$.dispatch(new Initialize());
+
+    // preloading data of the rest of the planets (low data volume and high chances it will be used)
+    // actualy should be done before displaying the planet list to display the first page according to some sorting criterion
+    this.storePlanetsData$.subscribe(planetsData => {
+      if (planetsData.state == 'iniData') this.store$.dispatch(new GetAll());
+    });
   }
 
   loadRest(): void {
-    this.store.dispatch(new GetAll())
+    this.store$.dispatch(new GetAll());
   }
 
 }
