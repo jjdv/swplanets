9925fad82683176d3cdd2cfb5dd6848cd6e7be75
Jacek Jakubik <36626288+jjdv@users.noreply.github.com>
2019-01-15 22:35:43 +0100

    proper map resizing

--- src/app/components/planet-list/map/sw-galaxy-map/sw-galaxy-map.component.ts
+++ src/app/components/planet-list/map/sw-galaxy-map/sw-galaxy-map.component.ts
@@ -1,12 +1,16 @@
-import { Component, Input, OnInit, ViewChild, ElementRef, DoCheck, AfterViewChecked, AfterContentChecked, AfterViewInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, ElementRef, DoCheck, AfterViewChecked, AfterContentInit, AfterContentChecked, AfterViewInit, HostListener } from '@angular/core';
 
 import { MapLetters, mapLetters, MapNumbers, mapNumbers } from './map-data'
 
-type TableStyle = {
+type TableStyle = null | {
   height: string;
   width?: string;
 }
-const hwRatio = 1080 / 1528;
+const hwRatio = 1080 / 1527;
+type MapStyle = null | {
+  'max-width': string;
+  'max-height': string;
+};
 
 
 @Component({
@@ -14,12 +18,37 @@ const hwRatio = 1080 / 1528;
   templateUrl: './sw-galaxy-map.component.html',
   styleUrls: ['./sw-galaxy-map.component.scss']
 })
-export class SwGalaxyMapComponent {
+export class SwGalaxyMapComponent implements OnInit {
   mapLetters: MapLetters = mapLetters;
   mapNumbers: MapNumbers = mapNumbers;
-  @Input() selectedId: string;
-  @ViewChild('mapImage') mapImage: ElementRef | null = null;
-  tableStyle: TableStyle | null = null;
+  @Input() selectedId: string = 'V18';
+  @ViewChild('container') container: ElementRef | null = null;
+  tableStyle: TableStyle = null;
+  mapStyle: MapStyle = null;
+
+  @HostListener('window:resize', ['$event'])
+  @HostListener('window:load', ['$event'])
+  setMapSize() {
+    const img = this.container.nativeElement.querySelector('img');
+    img.classList.add('no-display');
+    //const mapSpace = this.container.nativeElement.getBoundingClientRect();
+    this.mapStyle = {
+      'max-width': this.container.nativeElement.clientWidth + 'px',
+      'max-height': this.container.nativeElement.clientHeight + 'px'
+    };
+    img.classList.remove('no-display');
+  }
+
+  ngOnInit() {
+    this.setMapSize();
+    //this.container.nativeElement.onresize = this.setMapSize;
+    //alert(`height: ${rect.height}px\nwidth: ${rect.width}px`);
+    //console.log('container.nativeElement', this.container.nativeElement);
+  }
+
+  ngOnChanges() {
+    this.setMapSize()
+  }
 
   /*setTableStyle(img: Element) {
     if (img) {
@@ -32,8 +61,8 @@ export class SwGalaxyMapComponent {
   }*/
   
   /*ngDoCheck() {
-    if (this.mapImage.nativeElement) {
-      const imgRect = this.mapImage.nativeElement.getBoundingClientRect();
+    if (this.container.nativeElement) {
+      const imgRect = this.container.nativeElement.getBoundingClientRect();
       if (imgRect.height && imgRect.width) {
         this.tableStyle = {
           height: imgRect.height + 'px',
