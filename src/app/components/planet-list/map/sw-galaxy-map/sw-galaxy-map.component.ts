1e43b9549fe721c42b27bca8cff2e81af6165cc6
Jacek Jakubik <36626288+jjdv@users.noreply.github.com>
2019-01-17 23:01:58 +0100

    code cleanup

--- src/app/components/planet-list/map/sw-galaxy-map/sw-galaxy-map.component.ts
+++ src/app/components/planet-list/map/sw-galaxy-map/sw-galaxy-map.component.ts
@@ -1,15 +1,10 @@
-import { Component, Input, ViewChild, ElementRef, DoCheck, HostListener } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef, HostListener } from '@angular/core';
 
 import { MapLetters, mapLetters, MapNumbers, mapNumbers } from './map-data'
 
-type TableStyle = null | {
-  transform: string;
-}
 const hwRatio = 1080 / 1527;
-type SizeStyle = null | {
-  width?: string;
-  height?: string;
-  transform?: string;
+type TransformStyle = {
+  transform: string;
 };
 
 
@@ -18,40 +13,23 @@ type SizeStyle = null | {
   templateUrl: './sw-galaxy-map.component.html',
   styleUrls: ['./sw-galaxy-map.component.scss']
 })
-export class SwGalaxyMapComponent implements DoCheck {
+export class SwGalaxyMapComponent {
   mapLetters: MapLetters = mapLetters;
   mapNumbers: MapNumbers = mapNumbers;
   @Input() selectedId: string = 'V18';
   @ViewChild('container') container: ElementRef;
-  @ViewChild('map') map: ElementRef;
   style: Element;
-  tableCellSize: SizeStyle;
-  mapSize: SizeStyle = {
-    width: '0px',
-    height: '0px'
-  };
+  mapSize: TransformStyle = { transform: 'scale(0)' };
 
   @HostListener('window:resize', ['$event'])
   @HostListener('window:load', ['$event'])
   setMapSize() {
-    this.map.nativeElement.style.display = 'none';
     const cWidth = this.container.nativeElement.clientWidth;
     const cHeight = this.container.nativeElement.clientHeight;
 
-    let mWidth, mHeight;
-    if (cHeight/cWidth > hwRatio) {
-      mWidth = cWidth;
-      mHeight = String(cWidth * hwRatio);
-    }
-    else {
-      mWidth = String(cHeight / hwRatio);
-      mHeight = cHeight;
-    }
+    let mWidth;
+    if (cHeight/cWidth > hwRatio) mWidth = cWidth;
+    else mWidth = cHeight / hwRatio;
     this.mapSize = { transform: `scale(${mWidth / 1527})` };
-    this.map.nativeElement.style.display = 'block';
-  }
-
-  ngDoCheck() {
-    this.setMapSize();
   }
 }
